eb77370a8e9f6cc8e01de19b89f07bdc9d1ba9f3
Usama Farooq <ocamafarooq.97@gmail.com>
2023-04-18 19:34:45 +0500

    task 2 files upload

--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 import time
+import os
+
+
+import pandas as pd
+from bs4 import BeautifulSoup
 
 
 def scroll_all_pages(driver):
@@ -15,7 +20,6 @@ def scroll_all_pages(driver):
 
 
 def extract_comments_information(soup):
-
     comments_container = soup.find_all('ytd-comment-thread-renderer')
     comments = []
 
@@ -28,7 +32,69 @@ def extract_comments_information(soup):
         thumbnail_url_tag = comment.find('yt-img-shadow', {"class": "style-scope ytd-comment-renderer no-transition"})
         if thumbnail_url_tag is not None:
             thumbnail_url = thumbnail_url_tag.find('img').get('src')
-        else:
-            thumbnail_url = None
-        comments.append([user_name, comment_text, comment_time, likes, thumbnail_url])
+
+        comments.append([
+            user_name, comment_text, comment_time, likes, thumbnail_url
+        ])
+
     return comments
+
+
+def create_channel_directory(channel_url):
+    channel_name = channel_url.split("/")[-2].replace('@', '')
+    channel_dir = os.path.join(os.getcwd(), channel_name)
+    if not os.path.exists(channel_dir):
+        os.mkdir(channel_dir)
+    return channel_dir
+
+
+def extract_all_video_links(soup):
+    video_links = []
+    for video in soup.find_all('ytd-rich-grid-media'):
+        video_url = 'https://www.youtube.com' + video.find('a', {'id': 'video-title-link'}).get('href')
+        video_links.append(video_url)
+    return video_links
+
+
+def extract_video_comments_info(video_link, channel_dir, driver):
+    driver.get(video_link)
+
+    time.sleep(5)
+
+    scroll_all_pages(driver)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    comments_data = extract_comments_information(soup)
+
+    video_name = soup.title.string.split(" - YouTube")[0]
+    video_title = video_name.replace('/', '-')
+
+    file_name = video_title + '.csv'
+    file_path = os.path.join(channel_dir, file_name)
+    data_frame = pd.DataFrame(comments_data, columns=[
+        'User Name', 'Comment Text', 'Comment Time', 'Likes', 'Thumbnail Url'
+    ])
+    data_frame.to_csv(file_path, index=False)
+    print(f"Extract Comments of video {video_title} |saved to CSV file| {file_name}")
+
+
+def extracting_channel_videos(channel_url, driver):
+    channel_url += "/videos"
+    driver.get(channel_url)
+
+    time.sleep(5)
+
+    scroll_all_pages(driver)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    channel_dir = create_channel_directory(channel_url)
+    video_links = extract_all_video_links(soup)
+
+    for video_link in video_links:
+        extract_video_comments_info(video_link, channel_dir, driver)
+
+    print("Data successfully saved to CSV files")
